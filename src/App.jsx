3b5ee8d1923f2c35b5d10f8c73c37e6da3b6f9ec
Neeraj Saini <asaini7788@gmail.com>
2025-03-30 08:28:34 +0530

    Revert "Develop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-import Habits from "./components/Habits/Habits";
-import Header from "./components/Header/Header";
-import InputForm from "./components/InputForm/InputForm";
-import { HabitsProvider } from "./contexts/HabitsContext";
 function App() {
-  return (
-    <HabitsProvider>
-      <div className="centered-container">
-        <InputForm />
-        <div className="bordered-container">
-          <Header />
-          <Habits />
-        </div>
-      </div>
-    </HabitsProvider>
-  );
+  return <></>;
 }
 
 export default App;
